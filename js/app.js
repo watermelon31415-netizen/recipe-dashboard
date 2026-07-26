317bd6cd8846ddc1a937889364d0eb8cf56532d8
watermelon31415-netizen <watermelon31415@gmail.com>
2026-07-26 10:26:18 -0400

    Refactor showRecipes function for clarity and efficiency

--- js/app.js
+++ js/app.js
@@ -1,46 +1,65 @@
-const recipeList = document.getElementById("recipeList");
-
-
 async function showRecipes(){
 
-    await loadRecipes();
+
+await loadRecipes();
 
 
-    recipeList.innerHTML = "";
+
+const recipeList =
+document.getElementById("recipeList");
 
 
-    recipes.forEach(recipe => {
 
+if(!recipeList){
 
-        recipeList.innerHTML += `
-
-        <div class="recipe-card">
-
-            <img src="${recipe.image}">
-
-            <h2>
-
-                <a href="recipe-detail.html?id=${recipe.id}">
-                    ${recipe.name}
-                </a>
-
-            </h2>
-
-            <p>
-                🍽 ${recipe.meal}
-            </p>
-
-            <p>
-                🏷 ${(recipe.tags || []).join(" ")}
-            </p>
-
-        </div>
-
-        `;
-
-    });
+return;
 
 }
+
+
+
+recipeList.innerHTML = "";
+
+
+
+recipes.forEach(recipe => {
+
+
+recipeList.innerHTML += `
+
+<div class="recipe-card">
+
+<img src="${recipe.image}">
+
+
+<h2>
+
+<a href="recipe-detail.html?id=${recipe.id}">
+${recipe.name}
+</a>
+
+</h2>
+
+
+<p>
+🍽 ${recipe.meal}
+</p>
+
+
+<p>
+🏷 ${(recipe.tags || []).join(" ")}
+</p>
+
+
+</div>
+
+`;
+
+});
+
+
+}
+
 
 
 showRecipes();
