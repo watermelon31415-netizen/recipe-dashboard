8b858dcce8e1c7653f34601e800187b430ea6a4b
watermelon31415-netizen <watermelon31415@gmail.com>
2026-07-27 01:18:59 -0400

    Update menu-control.js

--- js/menu-control.js
+++ js/menu-control.js
@@ -1,14 +1,23 @@
 async function controlMenu(){
 
 
-const session =
-await checkAuth();
+const {
+data:{
+session
+}
+}=await supabaseClient.auth.getSession();
 
 
 
 const addBtn =
 document.getElementById(
 "addRecipeBtn"
+);
+
+
+const authBtn =
+document.getElementById(
+"authBtn"
 );
 
 
@@ -20,6 +29,40 @@ if(!session){
 addBtn.style.display="none";
 
 }
+
+}
+
+
+
+// 登录状态
+
+if(authBtn){
+
+
+if(session){
+
+
+authBtn.innerHTML =
+"🚪 Logout";
+
+
+authBtn.href="#";
+
+
+authBtn.onclick = async function(){
+
+
+await supabaseClient.auth.signOut();
+
+
+location.href="login.html";
+
+
+};
+
+
+}
+
 
 }
 
